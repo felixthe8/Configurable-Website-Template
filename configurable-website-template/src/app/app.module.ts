518f15e2dbda547fe43dcaf5a0729e1c3769b39e
Felix H <felixthe8@gmail.com>
2020-09-07 18:38:31 -0700

    Adding Angular Material to the project

--- configurable-website-template/src/app/app.module.ts
+++ configurable-website-template/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleBarComponent } from './shared/title-bar/title-bar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomepageComponent } from './homepage/homepage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
